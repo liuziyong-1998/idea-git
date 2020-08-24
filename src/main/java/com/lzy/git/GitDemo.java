45bc8e200944ceeee0d70a08a77bfadf8432e5d8
lzy <www.914894578@outlook.com>
2020-08-24 17:10:29 +0800

    idea第一次提交

---
+++ src/main/java/com/lzy/git/GitDemo.java
@@ -0,0 +1,13 @@
+package com.lzy.git;
+
+/**
+ * @Description:null
+ * @Author:lzy
+ * @Date:2020/8/24 17:07
+ * @Version:1.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
